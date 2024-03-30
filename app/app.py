9b246a805179464bd968ae677794cb3e7cdec222
Grigorii Tarasov <grigoriy.tarasov.u@gmail.com>
2024-03-30 13:31:27 +0400

    black-jupyter formated

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
-__doc__='''
+__doc__ = """
 uvicorn app.main:app --reload --port 8000
-'''
+"""
 
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
@@ -16,7 +16,7 @@ import uvicorn
 
 import output_converter
 
-sys.path.append('../')
+sys.path.append("../")
 
 from data_validation import handle_input_pic, handle_input_video
 from src.models.inference import PipelinePredictor
@@ -24,28 +24,35 @@ from src.models.inference import PipelinePredictor
 app = FastAPI()
 
 predictor = PipelinePredictor()
-def predict_one_frame(frame:np.array,
-                      predictor:PipelinePredictor)->List[Dict]:
+
+
+def predict_one_frame(frame: np.array, predictor: PipelinePredictor) -> List[Dict]:
     extraction_obj, age_preds = predictor.predict_img(frame)
-        
+
     prediction = []
-    
-    for face_obj, age_pred in zip(extraction_obj,
-                                  age_preds):
-        prediction.append({**output_converter.extract_to_dct(face_obj),
-                           **output_converter.age_to_dct(age_pred)})
-        
-    #print(prediction)     
+
+    for face_obj, age_pred in zip(extraction_obj, age_preds):
+        prediction.append(
+            {
+                **output_converter.extract_to_dct(face_obj),
+                **output_converter.age_to_dct(age_pred),
+            }
+        )
+
+    # print(prediction)
     return prediction
+
 
 # # Web
 
-@app.on_event('startup')
+
+@app.on_event("startup")
 def prepare_pipeline():
     print("Loading model...")
     predictor.load_model()
     print("Loaded succesivly...")
-    #return model
+    # return model
+
 
 @app.get("/ping")
 async def ping():
@@ -60,39 +67,33 @@ async def predict_photo(file: UploadFile = File(...)):
     try:
         image_np = await handle_input_pic(file)
     except Exception as e:
-        return JSONResponse(status_code=400, 
-                            content={"message": f'Error in input data: {e}'})
-    
-    prediction = predict_one_frame(image_np,
-                                   predictor)
-    
-    return JSONResponse(status_code=200, 
-                        content=prediction)
+        return JSONResponse(
+            status_code=400, content={"message": f"Error in input data: {e}"}
+        )
+
+    prediction = predict_one_frame(image_np, predictor)
+
+    return JSONResponse(status_code=200, content=prediction)
 
 
 @app.post("/api/predict_video")
-async def predict_video(
-    file: UploadFile = File(...)
-):
+async def predict_video(file: UploadFile = File(...)):
     try:
         frames = await handle_input_video(file)
     except Exception as e:
-        return JSONResponse(status_code=400, content={"message": f'Error in input data: {e}'})
-    
+        return JSONResponse(
+            status_code=400, content={"message": f"Error in input data: {e}"}
+        )
+
     predictions = []
-    for i,frame in enumerate(frames):
-        frame_dict = dict(frame_ind = i)
+    for i, frame in enumerate(frames):
+        frame_dict = dict(frame_ind=i)
         prediction = predict_one_frame(frame, predictor)
-        predictions.append({**frame_dict,
-                            **dict(prediction = prediction)
-                            })
-    
+        predictions.append({**frame_dict, **dict(prediction=prediction)})
+
     print(predictions)
-    return JSONResponse(status_code=200, 
-                        content=predictions)
+    return JSONResponse(status_code=200, content=predictions)
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
-
-
-
+    uvicorn.run(app, host="localhost", port=8000)
--- src/models/tf_mem.py
+++ src/models/tf_mem.py
@@ -1,12 +1,18 @@
 import tensorflow as tf
 
-def limit_gpu_gb(gb: float)->None:
-    '''Sets for all gpus max mem used to gb. Prevents GPU mem overflowing. Used for libs thats consumes >7Gb on <8Gb systems'''
-    
-    gpus = tf.config.experimental.list_physical_devices('GPU')
+
+def limit_gpu_gb(gb: float) -> None:
+    """Sets for all gpus max mem used to gb. Prevents GPU mem overflowing. Used for libs thats consumes >7Gb on <8Gb systems"""
+
+    gpus = tf.config.experimental.list_physical_devices("GPU")
 
     if gpus:
         for gpu in gpus:
             tf.config.experimental.set_virtual_device_configuration(
-                gpu, 
-                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=gb*1024)])
+                gpu,
+                [
+                    tf.config.experimental.VirtualDeviceConfiguration(
+                        memory_limit=gb * 1024
+                    )
+                ],
+            )
